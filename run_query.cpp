f489d322527c614a12e7e3a67829a483561bfa1d
Danny Cho <dannycho7@gmail.com>
2018-02-26 17:46:22 -0800

    Add docid to query result

--- run_query.cpp
+++ run_query.cpp
@@ -1,5 +1,17 @@
-
+#include <iostream>
+#include <vector>
+#include <indri/QueryEnvironment.hpp>
 
 int main() {
-
+	indri::api::QueryEnvironment env;
+	env.addIndex("./indri_cli/index");
+	
+	std::vector<indri::api::ScoredExtentResult> results = env.runQuery("international organized crime", 10);
+	std::vector<std::string> documentNames = env.documentMetadata(results, "docno");
+	
+	for(int i = 0; i < 10; i++) {
+		double score = results[i].score;
+		std::string docName = documentNames[i];
+		std::cout << docName << " " <<results[i].score <<std::endl;
+	}
 }
