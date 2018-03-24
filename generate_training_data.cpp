99f404140a85cb5f9a9123e85fb418cc18ca0419
Danny Cho <dannycho7@gmail.com>
2018-03-24 02:48:39 -0700

    Add qrels file parsing for relevance output on per qid-docid pair

--- generate_training_data.cpp
+++ generate_training_data.cpp
@@ -1,29 +1,62 @@
 #include <cmath>
 #include <cstdlib>
 #include <cstdio>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <set>
 #include <vector>
 #include <indri/QueryEnvironment.hpp>
 #include "ReScoringEnvironment.h"
 
+void generateRelDocs(std::string filename, std::set<std::string> &reldocs) {
+	std::ifstream qrelsfile;
+	qrelsfile.open(filename.c_str());
+
+	if (!qrelsfile) {
+		std::cerr << "Incorrect qrels filename" << std::endl;
+		exit(1);
+	}
+
+	std::string qrel_line;
+	while (std::getline(qrelsfile, qrel_line)) {
+		std::string docid;
+		int relevance, qid, ignore;
+		
+		std::stringstream xtract_qrel(qrel_line);
+		xtract_qrel >> qid >> ignore >> docid >> relevance;
+		if (relevance == 1) {
+			std::ostringstream reldocs_key;
+			reldocs_key << qid << "|" << docid;
+			reldocs.insert(reldocs_key.str());
+		}
+	}
+
+}
+
 int main(int argc, char *argv[]) {
 	int top_k;
+	std::string qrels_filename;
 
-	if (argc < 2) {
-		std::cerr << "Wrong Arguments. Correct Usage: ./run_query <top_k> < <queries_file_name>" << std::endl;
+	if (argc < 3) {
+		std::cerr << "Wrong Arguments. Correct Usage: ./run_query <top_k> <qrels_filename> < <queries_file_name>" << std::endl;
 		exit(1);
 	} else {
 		top_k = atoi(argv[1]);
+		qrels_filename = argv[2];
 	}
+
+	std::set<std::string> reldocs;
+	generateRelDocs(qrels_filename, reldocs);
 
 	indri::api::QueryEnvironment env;
 	env.addIndex("./indri_cli/index");
 	std::string in;
-	
+		
 	while(std::getline(std::cin, in)) {
 		std::string buf, query;
-		int qid, numTerms = 1, rel = 0;
+		int qid, numTerms = 1;
 
 		std::stringstream in_stream(in);
 		in_stream >> qid;
@@ -42,12 +75,21 @@ int main(int argc, char *argv[]) {
 		SCOREOUTPUT *bm25Scores = scorer->bm25(query, numTerms);
 
 		for(int i = 0; i < results.size(); i++) {
+			int rel = 0;
 			double score = results[i].score;
 			const char *docName = documentNames[i].c_str();
 			
 			double totalScore = bm25Scores[i].totalScore + tfIdfScores[i].totalScore + bm25Scores[i].maxScore + tfIdfScores[i].maxScore + bm25Scores[i].avgScore + tfIdfScores[i].avgScore;
 
 			if (isnan(totalScore)) continue;
+
+			std::ostringstream reldocs_key;
+
+			reldocs_key << qid << "|" << docName;
+
+			if (reldocs.find(reldocs_key.str()) != reldocs.end()) {
+				rel = 1;
+			}
 
 			printf("%i qid:%i 1:%.5f 2:%.5f 3:%.5f 4:%.5f 5:%.5f 6:%.5f #%s \n",
 				rel,
