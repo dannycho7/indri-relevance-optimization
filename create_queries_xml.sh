0965612b4f76023e8a27e90efdb4f7e2837757d2
Danny Cho <dannycho7@gmail.com>
2018-03-26 16:40:03 -0700

    Add create queries script for generating query list XML (for IndriRunQuery API) from query list file

---
+++ create_queries_xml.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+if [ $# -ne 2 ]; then
+	echo "Correct Usage: ./create_queries_xml.sh <index_file_path> <query_file_path>"
+	exit
+fi
+
+echo "<parameters>"
+echo "	<index>$1</index>"
+echo "	<count>10</count>"
+echo "	<trecFormat>true</trecFormat>"
+
+awk '{print "\t<query>"; print "\t\t<number>"$1"</number>"; $1 = ""; print "\t\t<text>"substr($0, 2)"</text>"; print "\t</query>"}' < $2
+
+echo "</parameters>"
